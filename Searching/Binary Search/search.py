dad0ea4f8f5221fd821291bf64c78e6d8383515e
Delowar <delowardev@gmail.com>
2020-01-22 21:29:09 +0600

    Binary Search Added

---
+++ Searching/Binary Search/search.py
@@ -0,0 +1,19 @@
+def binary_search(needle, haystack):
+	low, high = 0, len(haystack)
+	found = False
+	while low <= high and not found:
+		middle = (low + high) // 2
+		middle_item = haystack[middle]
+		if middle_item == needle:
+			found = True
+		else:
+			if needle < middle_item:
+				high = middle - 1
+			else: 
+				low = middle + 1
+
+	
+	return found
+
+
+print(binary_search(1, [1,2,3,4,5,6]))
--- Searching/Linear Search/search.py
+++ Searching/Linear Search/search.py
@@ -3,3 +3,6 @@ def linear_search(needle, haystack):
 		if(item[1] == needle):
 			return item[0]
 	return -1
+
+linear_search('c', ['a', 'b', 'c', 'd'])
+# linear_search('e', ['a', 'b', 'c', 'd']) // return -1
