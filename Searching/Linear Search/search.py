87d43553699d6117ab4d4f10cfbe148640229c7e
Delowar <delowardev@gmail.com>
2020-04-12 10:57:57 +0600

    Binary Search Added

--- Searching/Linear Search/search.py
+++
@@ -1,9 +0,0 @@
-def linear_search(needle, haystack):
-	for item in enumerate(haystack):
-		if(item[1] == needle):
-			return item[0]
-	return -1
-
-"""
-linear_search('e', ['a', 'b', 'c', 'd']) # return -1
-"""
